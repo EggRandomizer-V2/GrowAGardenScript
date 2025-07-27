7736b7d6683c0d3befbb1e7b891033f05c4fdd73
EggRandomizer-V2 <promptedtales@gmail.com>
2025-07-27 18:15:29 +0800

    Create EggRandomizer.lua

---
+++ EggRandomizer.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://pastefy.app/V4e1sFnM/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EggRandomizer-V2/GrowAGardenScript/refs/heads/main/EggDetectorXPetMutationRandomizer.lua"))()
